321bc9f3731bad9132bfdee2812ceb6db5f51b21
BryceHanke <Crazy707707@gmail.com>
2025-05-14 12:10:40 -0700

    first commit

---
+++ src/main/java/net/bryce/do_marijuana/Lets_do_marijuanaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bryce.do_marijuana;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Lets_do_marijuanaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
